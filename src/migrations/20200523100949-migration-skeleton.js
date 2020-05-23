666caee576343c4b215dbed31061de43b6c95b5f
Yatin Mehandiratta <iamyatin@gmail.com>
2020-05-23 16:18:17 +0530

    Setup Migrations via CLI

---
+++ src/migrations/20200523100949-migration-skeleton.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   await queryInterface.createTable('Tasks', {
+     guid: {
+       type: Sequelize.DataTypes.UUID,
+       defaultValue: Sequelize.DataTypes.UUIDV4,
+       primaryKey: true
+     },
+     title: {
+       type: Sequelize.DataTypes.STRING(50),
+       allowNull: false
+     },
+     done: {
+       type: Sequelize.DataTypes.BOOLEAN,
+       defaultValue: false,
+       allowNull: false
+     }
+   })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+   await queryInterface.dropTable('Tasks');
+  }
+};
